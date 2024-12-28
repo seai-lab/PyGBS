1acdec725c5e884014b581d48ef12b9ba900a38f
Wang Zhangyu <zhangyu-17@outlook.com>
2024-12-27 21:41:48 -0600

    update sri

--- example_code.py
+++ example_code.py
@@ -9,6 +9,8 @@ presence_values = np.random.choice([-1, 1], presence_points.shape[0], p=[0.25, 0
 
 # for density in [8000, 12000, 16000, 20000]:
 for density in np.array([8000, 12000, 16000, 20000]):
+    ## pnp_ssi is the former base geo-bias
     pnp_ssi = compute_presence_nonpresence_ssi(presence_points, center, radius, density, k=4)
+    ## rp_ssi is the former relative geo-bias
     rp_ssi = compute_relative_performance_ssi(presence_points, presence_values, center, radius, density, k=4)
     print("Presence v.s. Non-Presence SSI Score: ", pnp_ssi, "Relative Performance SSI Score: ", rp_ssi)
---
+++ gbs/sri/sri_scores.py
@@ -0,0 +1,10 @@
+
+
+def compute_scale_sri():
+    pass
+
+def compute_distance_sri():
+    pass
+
+def compute_direction_sri():
+    pass
